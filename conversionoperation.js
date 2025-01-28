2e7629b3375b1f88405717308e7e0a2ef608e67f
prince311206 <princepriyaranjan1234@gamil.com>
2025-01-28 11:17:46 +0530

    all co

--- conversionoperation.js
+++ conversionoperation.js
@@ -1,5 +1,42 @@
-let score = 30
-
+let score = "30"
 
 console.log(typeof score)
+
 console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let Score = "30abc"
+
+console.log(typeof Score)
+
+console.log(typeof(Score))
+
+let ValueInNumber = Number(Score)
+console.log(ValueInNumber);
+
+
+let core = null
+
+console.log(typeof core)
+
+console.log(typeof(core))
+
+let outInNumber = Number(core)
+console.log(typeof outInNumber);
+
+
+let scor = undefined
+
+console.log(typeof scor)
+
+console.log(typeof(scor))
+
+let OutInNumber = Number(scor)
+console.log(typeof OutInNumber);
+
+
+let nac = 1
+let booleannac = Boolean(nac)
+console.log(booleannac);
