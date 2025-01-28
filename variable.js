fd004a606b40651f6a6b187d42ee281f989142fd
prince311206 <princepriyaranjan1234@gamil.com>
2025-01-28 10:47:28 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountGmail = "princepriyaranjan1234@gmail.com"
+let accountname = "PRINCE PRIYARANJAN "
+let accountpassword = "311206"
+accountCity = "BERHAMPUR"
+
+accountGmail ="prateek1234gmail.com"
+accountname = "PRATEEK MOHANTY"
+accountpassword = "123456"
+accountCity = "DHENKANAL"
+console.log(accountId);
+console.table([accountId,accountGmail,accountname,accountpassword,accountCity]);
