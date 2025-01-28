cfb573af5843507ee537fb18868c9aff6e237166
prince311206 <princepriyaranjan1234@gamil.com>
2025-01-28 11:56:57 +0530

    all co

---
+++ newco.js
@@ -0,0 +1,25 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2+2);
+console.log(2/2);
+console.log(2**2);
+console.log(2*2);
+console.log(2-2);
+console.log(2+2);
+console.log(2%2);
+let str1 = "hello"
+let str2 = "prince"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log( 1 + 2 + "2" );
+// console.log(+true);  not to do
+// console.log(+"");    not to do
+// let n1,n2,n3         not to do
+// n1=n2=n3=2+2         not to do
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
