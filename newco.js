d3cd20388f5857275d6fc08deeb41ecd2f0b0841
prince311206 <princepriyaranjan1234@gamil.com>
2025-01-28 12:10:23 +0530

    compare datatype

--- newco.js
+++ newco.js
@@ -9,7 +9,7 @@ console.log(2-2);
 console.log(2+2);
 console.log(2%2);
 let str1 = "hello"
-let str2 = "prince"
+let str2 = " prince"
 let str3 = str1 + str2
 console.log(str3);
 console.log("1" + 2);
