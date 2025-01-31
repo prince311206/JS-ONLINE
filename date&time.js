3a14ad553470c9ccd198c2a81255c7b2666fd7e1
prince311206 <princepriyaranjan1234@gamil.com>
2025-01-31 11:16:27 +0530

    date and time

---
+++ date&time.js
@@ -0,0 +1,11 @@
+let dt = new Date()
+
+console.log(dt);
+console.log(dt.toString());
+console.log(dt.toLocaleString());
+console.log(dt.toLocaleDateString());
+console.log(dt.toDateString());
+console.log(dt.toTimeString());
+console.log(typeof dt);
+
+console.log(Math.floor(Date.now()/1000));
